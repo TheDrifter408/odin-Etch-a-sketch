68470ffa7cac8fb87a33bbbf4f01f218e76b20e4
TheDrifter408 <nurullahan408@gmail.com>
2024-09-07 18:58:02 +0600

    decreased the gap in the flex containers, modified the event listeners with 'mouseenter' event

---
+++ script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('#container');
+
+const button = document.createElement("button");
+button.textContent = "New Grid";
+container.appendChild(button);
+button.addEventListener('click',(e) => {
+    let input = prompt("Number of Squares for each side: ");
+    while(isNaN(Number(input)) || Number(input) > 100 ){
+        input = prompt("Number cannot be greater than 100: ")
+    }
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    createGrid(input);
+})
+
+//function to create a container of row of 16 cells
+function Rows(columns){
+    const row = document.createElement('div');
+    row.setAttribute('id','row');
+    for(let i=0;i<columns;i++){
+        const cell = document.createElement('div');
+        cell.addEventListener('mouseenter',(e) => {
+            e.target.style.backgroundColor = 'lightblue';
+        })
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+    return row;
+}
+
+function createGrid(rows = 16){
+    container.appendChild(button);
+    for(let i=0;i<rows;i++){
+       container.appendChild(Rows(rows)); 
+    }
+}
+
+createGrid();
